130dc126d8874bc6c45ad10fc31a62a72d344ea1
M.Hitesh <54840773+hiteshmaddy123@users.noreply.github.com>
2021-08-05 18:16:18 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -13,22 +13,11 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    
-    # For rendering results on HTML GUI
-    
-    #int_features = [int(x) for x in request.form.values()]
-    #final_features = [np.array(int_features)]
-    #prediction = model.predict(final_features)
-
-    #output = round(prediction[0], 2)
-
-    #return render_template('index.html', prediction_text='PM2.5 AQI Value should be {}'.format(output))
 
     df=pd.read_csv('xtest.csv')
     my_prediction=model.predict(df.values)
     my_prediction=my_prediction.tolist()
     return render_template('result.html',prediction = my_prediction)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
